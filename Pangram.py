967d88470461950cb6a46018e2c0a2dcc7099715
bodalapallepavansai <69682199+bodalapallepavansai@users.noreply.github.com>
2020-08-14 20:28:18 +0530

    Pangram.py

---
+++ Pangram.py
@@ -0,0 +1,23 @@
+import string
+
+def ispangram(sentence):
+    return (set(sentence) >= alphabets)# String Comparison
+
+fhandle=open(input("Enter the name of the file:"))
+
+alphabets = set(string.ascii_lowercase)
+# set() of alphabets
+
+count=0
+
+for line in fhandle:
+    pangram = ispangram(line.rstrip().lower())
+    if pangram == True:
+        print(" Line "+str(int(count))+" : "+str(line)+" "+"is a Pangram")
+    count+=0.5
+    print()
+        
+
+def ispangram(sentence):
+    return (set(sentence) >= alphabets)# String Comparison
+
